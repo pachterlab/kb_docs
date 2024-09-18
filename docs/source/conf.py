ffb4027bde4e72056d21d02bf1f495d2a7c6dc2f
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2024-09-17 17:57:55 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,12 +39,5 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-rst_prolog = """
-.. role:: raw-text(literal)
-   :class: raw
-
-.. |--| replace:: ``--``
-"""
-
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
