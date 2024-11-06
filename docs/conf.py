1bd1b924e343625de9fa939ad8aa0131eef563c7
Laura Luebbert, Ph.D. <56094636+lauraluebbert@users.noreply.github.com>
2024-11-06 23:22:31 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+# Enable the GitHub link
+html_context = {
+    "display_github": True,
+    "github_user": "pachterlab",
+    "github_repo": "kb_docs",
+    "github_version": "main",
+    "conf_py_path": "/docs/",
+}
