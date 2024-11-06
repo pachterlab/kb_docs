dc4a33159f74e203e54d1c333837f5ad8edc0066
Laura Luebbert, Ph.D. <56094636+lauraluebbert@users.noreply.github.com>
2024-11-07 00:14:39 +0100

    Delete docs/conf.py

--- docs/conf.py
+++
@@ -1,8 +0,0 @@
-# Enable the GitHub link
-html_context = {
-    "display_github": True,
-    "github_user": "pachterlab",
-    "github_repo": "kb_docs",
-    "github_version": "main",
-    "conf_py_path": "/docs/",
-}
